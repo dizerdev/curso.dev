a516e2463bec7623800471647b5787f6467942c9
Diego Santos <106088657+dizerdev@users.noreply.github.com>
2025-04-25 21:34:05 +0000

    Add `/api/v1/migrations` endpoint and a few refactoring

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,18 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDataBase);
+
+async function cleanDataBase() {
+  await database.query("DROP SCHEMA public cascade; CREATE SCHEMA public;");
+}
+
+test("GET to api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
